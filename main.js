a6d06117429cc581082378f093dc835a1b39194e
Micheni58 <michenicaleb@gmail.com>
2025-06-18 12:16:25 +0300

    Modify main.js

--- main.js
+++ main.js
@@ -3,10 +3,34 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const errorModal= document.getElementById('modal');
+errorModal.classList.add('hidden');
 
+const articleHearts = document.querySelectorAll('.like-glyph');
+// console.log(articleHearts)
+for(const glyph of articleHearts){
+  glyph.addEventListener('click', updateLike);
 
+}
+function updateLike(e){
+  const heart = e.target;
+  mimicServerCall()
+  .then(()=> {
+    if (heart.innerText === EMPTY_HEART ){
+      heart.innerText = FULL_HEART;
+      heart.classList.add('activated-heart');
+    }
+  })
+  .catch((error) => {
+    errorModal.classList.remove('hidden');
+    const modalMessage = document.getElementById('modal-message');
+    modalMessage.innerText = error;
 
-
+    setTimeout(() => {
+      errorModal.classList.add('hidden');
+    },3000);
+  })
+}
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
